e55e28c0f25e328be64c2fe210ee1e010df76c18
FarrVel <farreltasya1@gmail.com>
2026-06-07 20:39:51 +0700

    Nambahin fitur collect and carry
    Sesuai fitur tapi masih bisa ngambang waktu di taruh

--- Mechanic/Interaction/pick_up_trash.cs
+++ Mechanic/Interaction/pick_up_trash.cs
@@ -3,29 +3,63 @@ using System;
 
 public partial class pick_up_trash : RayCast3D
 {
+	[Export] public Node3D PosisiPegang;
+
+	private Node3D _objekDiPegang = null;
+
 	public override void _Ready()
 	{
-		Enabled = true; 
+		Enabled = true;
 	}
 
 	public override void _Input(InputEvent @event)
 	{
 		if (@event.IsActionPressed("interact"))
 		{
-			if (IsColliding())
+			if (_objekDiPegang == null)
 			{
-				Node3D objekTertabrak = (Node3D)GetCollider();
-
-				if (objekTertabrak.IsInGroup("Sampah"))
+				if (IsColliding())
 				{
-					GD.Print("Memungut sampah: " + objekTertabrak.Name);
-					objekTertabrak.QueueFree();
+					Node3D target = (Node3D)GetCollider();
+					if (target.IsInGroup("Sampah"))
+					{
+						AmbilObjek(target);
+					}
 				}
 			}
 			else
 			{
-				GD.Print("Tidak ada objek di depan.");
+				LepasObjek();
 			}
 		}
+	}
+
+	private void AmbilObjek(Node3D target)
+	{
+		GD.Print("Membawa sampah: " + target.Name);
+		_objekDiPegang = target;
+
+		_objekDiPegang.Reparent(PosisiPegang);
+
+		_objekDiPegang.Position = Vector3.Zero;
+
+		if (_objekDiPegang is CsgShape3D kotakSampah)
+		{
+			kotakSampah.UseCollision = false;
+		}
+	}
+
+	private void LepasObjek()
+	{
+		GD.Print("Melepas sampah: " + _objekDiPegang.Name);
+
+		_objekDiPegang.Reparent(GetTree().CurrentScene);
+
+		if (_objekDiPegang is CsgShape3D kotakSampah)
+		{
+			kotakSampah.UseCollision = true;
+		}
+
+		_objekDiPegang = null;
 	}
 }
