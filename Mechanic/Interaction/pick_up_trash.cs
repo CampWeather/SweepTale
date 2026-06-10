2d7dd32da310f913c5fc58ed34dd379f737111ff
FarrVel <farreltasya1@gmail.com>
2026-06-10 16:19:20 +0700

    Carry and SaveInventory

--- Mechanic/Interaction/pick_up_trash.cs
+++ Mechanic/Interaction/pick_up_trash.cs
@@ -3,29 +3,61 @@ using System;
 
 public partial class pick_up_trash : RayCast3D
 {
+	[Export] public Node3D HandPosition; 
+	
+	private Node3D heldObject = null;
+
 	public override void _Ready()
 	{
 		Enabled = true; 
 	}
+	
+	public override void _Process(double delta)
+	{
+		if (heldObject != null && HandPosition != null)
+		{
+			heldObject.GlobalTransform = HandPosition.GlobalTransform;
+		}
+	}
 
 	public override void _Input(InputEvent @event)
 	{
-		if (@event.IsActionPressed("interact"))
+		if (@event.IsActionPressed("interact") && heldObject == null)
 		{
 			if (IsColliding())
 			{
-				Node3D objekTertabrak = (Node3D)GetCollider();
-
-				if (objekTertabrak.IsInGroup("Sampah"))
+				Node3D collider = (Node3D)GetCollider();
+				
+				if (collider.IsInGroup("CarryableTrash"))
 				{
-					GD.Print("Memungut sampah: " + objekTertabrak.Name);
-					objekTertabrak.QueueFree();
+					PickUpObject(collider);
 				}
 			}
-			else
-			{
-				GD.Print("Tidak ada objek di depan.");
-			}
 		}
+
+		if (@event.IsActionPressed("store_item") && heldObject != null)
+		{
+			StoreToInventory();
+		}
+	}
+
+	private void PickUpObject(Node3D obj)
+	{
+		GD.Print("Memegang objek: " + obj.Name);
+		heldObject = obj;
+
+		if (heldObject is RigidBody3D rb)
+		{
+			rb.Freeze = true;
+			rb.CollisionLayer = 0;
+			rb.CollisionMask = 0;
+		}
+	}
+
+	private void StoreToInventory()
+	{
+		GD.Print("Sukses menyimpan " + heldObject.Name + " ke inventory.");
+		heldObject.QueueFree(); 
+		heldObject = null; 
 	}
 }
