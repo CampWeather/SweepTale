7fbdc645c6a68c99f0d850ac8b7890d8916b2102
FarrVel <farreltasya1@gmail.com>
2026-06-10 16:42:25 +0700

    Update Carry and Store + SaveInventory
    Carry = E
    Store/Lempar = Hold E
    Save Inventory = F

--- Mechanic/Interaction/pick_up_trash.cs
+++ Mechanic/Interaction/pick_up_trash.cs
@@ -7,37 +7,87 @@ public partial class pick_up_trash : RayCast3D
 	
 	private Node3D heldObject = null;
 
+	private uint originalLayer;
+	private uint originalMask;
+
+	private float eHoldTimer = 0.0f;
+	private float throwHoldThreshold = 0.3f; 
+	private bool isTrackingHold = false;
+	private bool justPickedUp = false; 
+
 	public override void _Ready()
 	{
 		Enabled = true; 
 	}
-	
+
 	public override void _Process(double delta)
 	{
 		if (heldObject != null && HandPosition != null)
 		{
 			heldObject.GlobalTransform = HandPosition.GlobalTransform;
 		}
+
+		if (heldObject != null)
+		{
+			if (justPickedUp)
+			{
+				if (Input.IsActionJustReleased("interact"))
+				{
+					justPickedUp = false;
+				}
+				return; 
+			}
+
+			if (Input.IsActionJustPressed("interact"))
+			{
+				isTrackingHold = true;
+				eHoldTimer = 0.0f;
+			}
+
+			if (isTrackingHold && Input.IsActionPressed("interact"))
+			{
+				eHoldTimer += (float)delta;
+				
+				if (eHoldTimer >= throwHoldThreshold)
+				{
+					ThrowObject();
+					isTrackingHold = false;
+					eHoldTimer = 0.0f;
+				}
+			}
+
+			if (Input.IsActionJustReleased("interact"))
+			{
+				isTrackingHold = false;
+				eHoldTimer = 0.0f;
+			}
+		}
 	}
 
 	public override void _Input(InputEvent @event)
 	{
-		if (@event.IsActionPressed("interact") && heldObject == null)
+		if (heldObject == null)
 		{
-			if (IsColliding())
+			if (@event.IsActionPressed("interact"))
 			{
-				Node3D collider = (Node3D)GetCollider();
-				
-				if (collider.IsInGroup("CarryableTrash"))
+				if (IsColliding())
 				{
-					PickUpObject(collider);
+					Node3D collider = (Node3D)GetCollider();
+					
+					if (collider.IsInGroup("CarryableTrash"))
+					{
+						PickUpObject(collider);
+					}
 				}
 			}
 		}
-
-		if (@event.IsActionPressed("store_item") && heldObject != null)
+		else
 		{
-			StoreToInventory();
+			if (@event.IsActionPressed("store_item"))
+			{
+				isTrackingHold = false; 
+				StoreToInventory();
+			}
 		}
 	}
 
@@ -48,10 +98,36 @@ public partial class pick_up_trash : RayCast3D
 
 		if (heldObject is RigidBody3D rb)
 		{
+			originalLayer = rb.CollisionLayer;
+			originalMask = rb.CollisionMask;
+
 			rb.Freeze = true;
 			rb.CollisionLayer = 0;
 			rb.CollisionMask = 0;
 		}
+
+		justPickedUp = true;
+		isTrackingHold = false;
+		eHoldTimer = 0.0f;
+	}
+
+	private void ThrowObject()
+	{
+		GD.Print("Melempar objek (Hold E): " + heldObject.Name);
+		
+		if (heldObject is RigidBody3D rb)
+		{
+			rb.Freeze = false;
+			rb.CollisionLayer = originalLayer;
+			rb.CollisionMask = originalMask;
+			
+			Vector3 throwDirection = -GlobalTransform.Basis.Z;
+			float throwForce = 15.0f; 
+			
+			rb.ApplyImpulse(throwDirection * throwForce);
+		}
+		
+		heldObject = null; 
 	}
 
 	private void StoreToInventory()
